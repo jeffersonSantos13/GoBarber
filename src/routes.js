ee10d318b2061b6eeaff0cbeb030ea0fa3a643ea
Jefferson Santos <yuiti.santos@gmail.com>
2020-02-16 23:58:37 -0300

    Criando as rotas para os Appointments

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
-import multerConfig from './config/multer';
 
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import Filecontroller from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,10 +18,25 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+/**
+ * Routes Userss
+ */
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), Filecontroller.store);
-
+/**
+ * routes providers
+ */
 routes.get('/providers', ProviderController.index);
+
+/**
+ * Routes Appointments
+ */
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+/**
+ * Routes Files
+ */
+routes.post('/files', upload.single('file'), Filecontroller.store);
 
 export default routes;
